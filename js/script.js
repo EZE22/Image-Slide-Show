8a41747aa8c88b9a4540b912b8d01f0e96c7698b
Adam Ingram <everett.ingram22@gmail.com>
2019-12-02 13:39:49 -0500

    Setting up GitHub

---
+++ js/script.js
@@ -0,0 +1,56 @@
+$(function() {
+  // jQuery goes here... 
+  $(".lightbox").fadeIn(1000);
+
+
+
+
+
+  //Delaying using callback funcitons
+  // $(".red-box").fadeTo(1000, 0, function() {
+  //   $(".green-box").fadeTo(1000, 0, function() {
+  //     $(".blue-box").fadeTo(1000, 0);
+  //   });
+  // });
+
+  // $(".red-box").fadeTo(1000, 0, function(){
+  //   alert("This is the callback funciton: Animation finished...")
+  // })
+
+
+  //Delaying animations using method channing
+  // $(".red-box").fadeTo(1000, 0.2);
+  // $(".green-box").delay(1000).fadeTo(1000, 0.5);
+  // $(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut().delay(500).fadeIn();
+
+  //custome animation
+  // $(".blue-box").animate({
+  //   "margin-left": "+=200px"
+  // }, 1000, "linear");
+
+  //More Customer Animations
+  // $("p").animate({
+  //   fontSize: "20px"
+  // }, 1000);
+
+  // $(".blue-box").animate({
+  //   marginLeft: "200px",
+  //   opacity: "0",
+  //   height: "50px",
+  //   width: "50px",
+  //   marginTop: "25px"
+  // }, 1000);
+
+  // $(".blue-box").slideToggle(2000);
+  // $("p").hide(1000);
+  // $("p").slideDown(1000);
+
+
+
+  // $(".red-box").fadeTo(1000, 0.2);
+  // $(".green-box").fadeTo(1000, 0.5);
+  // $(".blue-box").fadeTo(1000, 0.8);
+
+  // $(".green-box").fadeToggle();
+  // $(".green-box").fadeToggle();
+});
